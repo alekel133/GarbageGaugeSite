0097f575e3f0d132d883351d478e4c9649beda80
Alexander Kellough <atk133@msstate.edu>
2024-09-08 21:41:02 -0500

    Update files having save issue. Should be correct now.

--- app/__init__.py
+++ app/__init__.py
@@ -7,20 +7,12 @@ from config import DevelopmentConfig
 db = SQLAlchemy()
 migrate = Migrate()
 
-def create_app():
-    """
-    @brief Creates and cofigures Garbage Gauge Flask App
 
-    @return A configured Flask application.
-    """
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
 
-    app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
+db.init_app(app)
+migrate.init_app(app, db)
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    # Register blueprints
-    app.register_blueprint(main_bp)
-
-    return app
+# Register blueprints
+app.register_blueprint(main_bp)
